1badd1f4b4f108aa37eb819d7b433a4bd826ae02
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-08-24 09:47:50 -0300

    feat: implementing react hook form in todoform

--- src/components/TodoForm/index.jsx
+++ src/components/TodoForm/index.jsx
@@ -1,8 +1,16 @@
+import { useForm } from "react-hook-form"
+
 export const TodoForm = () => {
+    const { register, handleSubmit } = useForm();
+
+    const submit = (formData) => {
+        console.log(formData);
+    }
+
     return(
-        <form>
-            <input type="text" />
-            <textarea></textarea>
+        <form onSubmit={handleSubmit(submit)}>
+            <input type="text" {...register("title")} />
+            <textarea {...register("content")}></textarea>
             <button type="submit">Cadastrar</button>
         </form>
     )
